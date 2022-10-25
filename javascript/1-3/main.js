0c7fc109bb2d824865e2ea861c6a84c0ff46db16
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-10-25 20:48:33 +0900

    学習履歴7

---
+++ javascript/1-3/main.js
@@ -0,0 +1,23 @@
+let a = 12;
+if (a !== 10) {
+    console.log(a);
+}
+
+let b = 13;
+if (b >= 10 && b < 20 && b % 2 === 0) {
+    console.log(b);
+}
+
+let x = 9;
+if (x >= 10 && x <= 20) {
+    console.log("成功です");
+} else {
+    console.log("失敗です");
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log("偶数です");
+} else if (y % 2 !== 0) {
+    console.log("奇数です");
+}
