91048d137e6f32b3667a489198e899befe5372f4
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-11-09 18:17:25 +0900

    first commit
    index.php
    header と footer

---
+++ PHPjob/4-2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="1599315827_logo.png" alt="logo" width="200px" height="120px">
+        <div>
+            <div class="welcome_message">
+                <p>ようこそ隼田翔吾さん</p>
+            </div>
+            <div class="login_timestamp">
+                <p>最終ログイン日：2019-07-02 21:56:05</p>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="table"></div>
+    </main>
+    <footer>
+        <p>Y&I group.inc</p>
+    </footer>
+</body>
+</html>
