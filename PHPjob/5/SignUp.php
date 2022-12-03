9a60fead3fbe5b59aeca72b0627aff6e39856af6
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-03 15:20:01 +0900

    データベース接続、新規登録

--- PHPjob/5/SignUp.php
+++ PHPjob/5/SignUp.php
@@ -2,33 +2,40 @@
 
 // db_connect.phpの読み込みFILL_IN
 require_once('db_connect.php');
-// メッセージの初期化
-$errorMessage = "";
-$signUpMessage = "";
 
 // POSTで送られていれば処理実行
 if (isset($_POST['signUp'])){
     $name = $_POST['name'];
     $password = $_POST['password'];
+    // nameとpassword両方送られてきたら処理実行
+    if (empty($name) || empty($password)){
+        echo "user or password is empty.";
+    }else{
+        // PDOのインスタンスを取得FILL_IN
+        try {
+            $dbh = connect();
+            // SQL文の準備 FILL_IN
+            $sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
+            // パスワードをハッシュ化
+            $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+            // プリペアドステートメントの作成 FILL_IN
+            $stmt = $dbh->prepare($sql);
+            // 値をセット FILL_IN
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':password', $password_hashed);
+            // 実行 FILL_IN
+            $stmt->execute();
+            // 登録完了メッセージ出力
+            echo("登録が完了しました。");
+        }catch (PDOException $e) {
+            // エラーメッセージの出力 FILL_IN
+            echo "Error: " . $e->getMessage();
+            // 終了 FILL_IN
+            die();
+        }
+    }
 };
-// nameとpassword両方送られてきたら処理実行
-if (empty($name)){
-    
-}
-// PDOのインスタンスを取得FILL_IN
-try {
-    
-// SQL文の準備 FILL_IN 
-// パスワードをハッシュ化
-// プリペアドステートメントの作成 FILL_IN 
-// 値をセット FILL_IN
-// 実行 FILL_IN
-// 登録完了メッセージ出力
-}catch (PDOException $e) {
-// エラーメッセージの出力 FILL_IN 
-// 終了 FILL_IN
-}
-}
+
 ?>
 
 
--- PHPjob/5/db_connect.php
+++ PHPjob/5/db_connect.php
@@ -1,11 +1,23 @@
 <?php
-// セッション開始
-session_start();
-// DBサーバのURL
-$db['host'] = "localhost";
-// ユーザー名
-$db['user'] = "root";
-// ユーザー名のパスワード
-$db['pass'] = "root";
-// データベース名
-$db['dbname'] = "yigroupBlog";
+    define('DB_DATABASE', 'yigroupBlog');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('PDO_DSN', 'mysql:host=localhost; charset=utf8; dbname='.DB_DATABASE);
+
+    // DBに接続する関数を定義
+    function connect(){
+        // 処理開始
+        try {
+            // 定数を用いてDBに接続
+            $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+            // データベースハンドルの属性をPDOExceptionを投げるよう変更
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        // 例外処理
+        } catch (PDOExceptiion $e) {
+            // エラーメッセージを取得して表示
+            echo 'Error' . $e->getMessgae();
+            // DBとの接続を終了
+            die();
+        }
+    }
