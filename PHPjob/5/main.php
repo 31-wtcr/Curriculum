834a340c5f6b68ee8982d48a769ddf26f1e0c44a
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-04 15:59:32 +0900

    Update main.php
    記事一覧表示

--- PHPjob/5/main.php
+++ PHPjob/5/main.php
@@ -1,6 +1,27 @@
 <?php
+// db_connect.phpの読み込み
+require_once ('db_connect.php');
+// function.phpの読み込み
 require_once ('function.php');
+// ログインしていなければ、login.phpにリダイレクト
 check_user_logged_in();
+// PDOのインスタンスを取得
+$dbh = connect();
+
+try{
+    // SQL文の準備
+    $sql = "SELECT * FROM posts";
+    // プリペアドステートメントの作成
+    $stmt = $dbh->prepare($sql);
+    // 実行
+    $stmt->execute();
+} catch(PDOException $e) {
+    // エラーメッセージの出力
+    echo "Error: " . $e->getMessage();
+    // 終了
+    die();
+}
+
 ?>
 <!doctype html>
 <html>
@@ -12,5 +33,18 @@ check_user_logged_in();
     <h1>メインページ</h1>
     <p>ようこそ<?php echo $_SESSION["user_name"]; ?>さん</p>
     <a href="logout.php">ログアウト</a>
+    <table>
+        <tr>
+            <th>記事ID</th><th>タイトル</th><th>本文</th><th>投稿日</th>
+        </tr>
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){ ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['content']; ?></td>
+                <td><?php echo $row['time']; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
