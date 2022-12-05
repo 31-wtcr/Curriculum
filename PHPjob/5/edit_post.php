22a8b87189ebcb407823d8f2510051010324c4cd
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-05 12:12:02 +0900

    投稿取得を関数化
    指定されたIDによる検索を関数として扱う。

--- PHPjob/5/edit_post.php
+++ PHPjob/5/edit_post.php
@@ -14,33 +14,11 @@ $id = $_GET['id'];
 // 不正なアクセス対策
 redirect_main_unless_parameter($id);
 
-// PDOのインスタンスを取得
-$pdo = connect();
-
-try {
-    // SQL文の準備
-    $sql = "SELECT * FROM posts WHERE id = :id";
-    // プリペアドステートメントの作成
-    $stmt = $pdo->prepare($sql);
-    // idのバインド
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
-} catch (PDOException $e) {
-    // エラーメッセージの出力
-    echo 'Error: ' . $e->getMessage();
-    // 終了
-    die();
-}
-
-// 結果が1行取得できたら
-if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $id = $row['id'];
-    $title = $row['title'];
-    $content = $row['content'];
-} else {
-    // 対象のidでレコードがない => 不正な画面遷移
-    echo "対象のデータがありません。";
-}
+$row = find_post_by_id($id);
+// 関数から取得した値を格納
+$id = $row['id'];
+$title = $row['title'];
+$content = $row['content'];
 ?>
 <!DOCTYPE html>
 <html>
--- PHPjob/5/function.php
+++ PHPjob/5/function.php
@@ -15,3 +15,30 @@ function redirect_main_unless_parameter($param){
         exit;
     }
 }
+
+function find_post_by_id($id){
+    // PDOのインスタンスを取得
+    $pdo = connect();
+
+    try {
+        // SQL文の準備
+        $sql = "SELECT * FROM posts WHERE id = :id";
+        // プリペアドステートメントの作成
+        $stmt = $pdo->prepare($sql);
+        // idのバインド
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        // エラーメッセージの出力
+        echo 'Error: ' . $e->getMessage();
+        // 終了
+        die();
+    }
+
+    // 結果が1行取得できたら
+    if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        return $row;
+    } else {
+        redirect_main_unless_parameter($row);
+    }
+}
