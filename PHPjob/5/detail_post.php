001e86d4f6bf896ad0c2a5e4f89e5c66776b571a
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-05 18:24:14 +0900

    コメント機能実装
    コメント投稿機能と記事詳細画面でのコメント一覧表示

--- PHPjob/5/detail_post.php
+++ PHPjob/5/detail_post.php
@@ -18,7 +18,26 @@ $row = find_post_by_id($id);
 // 関数から取得した値を格納
 $id = $row['id'];
 $title = $row['title'];
-$content = $row['content'];?>
+$content = $row['content'];
+
+// pdoインスタンスを取得
+$pdo_comments = connect();
+
+try {
+    // SQL文の準備
+    $sql_comments = "SELECT * FROM comments WHERE post_id = :post_id";
+    // プリペアドステートメントの作成
+    $stmt_comments = $pdo_comments->prepare($sql_comments);
+    // idのバインド
+    $stmt_comments->bindParam(':post_id', $id);
+    $stmt_comments->execute();
+} catch (PDOException $e) {
+    // エラーメッセージの出力
+    echo 'Error: ' . $e->getMessage();
+    // 終了
+    die();
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -42,5 +61,14 @@ $content = $row['content'];?>
         </table>
         <a href="create_comment.php?post_id=<?php echo $id ?>">この記事にコメントする</a><br />
         <a href="main.php">メインページに戻る</a>
+        <?php
+        // 結果が一行取得できたら
+        while ($row = $stmt_comments->fetch(PDO::FETCH_ASSOC)){
+            echo '<hr>';
+            echo $row['name'];
+            echo '<br />';
+            echo $row['content'];
+        }
+        ?>
     </body>
 </html>
