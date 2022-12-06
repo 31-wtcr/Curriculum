eec224c94e3cdf17e389c3a9041715c6c7b4658f
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-06 18:30:08 +0900

    Update signUp.php
    7行目returnでhtmlが表示されない問題を修正

--- PHPjob/5/チェックテスト/signUp.php
+++ PHPjob/5/チェックテスト/signUp.php
@@ -2,44 +2,45 @@
 // function.phpの読み込み
 require_once('function.php');
 
-// $_POSTが空であれば処理を終了する
-if(empty($_POST['signUp'])){
-    return;
-// ユーザー名が空であれば警告を表示
-}elseif(empty($_POST['username'])) {
-    echo 'Username is empty.';
-// パスワードが空であれば警告を表示
-}elseif (empty($_POST['password'])) {
-    echo 'Password is empty.';
-
-// ユーザー名とパスワードが空でなければ以下の処理を続行
-}elseif(!empty($_POST['username']) && !empty($_POST['password'])){
-    // ユーザー名とパスワードのエスケープ処理
-    $username = htmlspecialchars($_POST['username'], ENT_QUOTES);
-    $password = htmlspecialchars($_POST['password'], ENT_QUOTES);
-    // ユーザー新規登録処理開始
-    $pdo = db_connect();
-    try {
-        // SQL文用意
-        $sql = 'INSERT INTO users (name, password) VALUES (:username, :password)';
-        // パスワードハッシュ化
-        $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-        // プリペアドステートメント化
-        $stmt = $pdo->prepare($sql);
-        // ユーザー名をバインド
-        $stmt->bindParam(':username', $username);
-        // パスワードをバインド
-        $stmt->bindParam(':password', $password_hashed);
-        // 実行
-        $stmt->execute();
-        // 登録完了メッセージ出力
-        echo('New user registered.');
-    // 例外処理
-    } catch (PDOException $e) {
-        //エラーメッセージ表示
-        echo 'Error: ' . $e->getMessage();
-        // 接続終了
-        die();
+// $_POSTが空でなければ処理をする
+if(!empty($_POST['signUp'])){
+    // ユーザー名が空であれば警告を表示
+    if(empty($_POST['username'])){
+        echo 'Username is empty.';
+    }
+    // パスワードが空であれば警告を表示
+    if (empty($_POST['password'])){
+        echo 'Password is empty.';
+    }
+    // ユーザー名とパスワードが空でなければ以下の処理を続行
+    if(!empty($_POST['username']) && !empty($_POST['password'])){
+        // ユーザー名とパスワードのエスケープ処理
+        $username = htmlspecialchars($_POST['username'], ENT_QUOTES);
+        $password = htmlspecialchars($_POST['password'], ENT_QUOTES);
+        // ユーザー新規登録処理開始
+        $pdo = db_connect();
+        try {
+            // SQL文用意
+            $sql = 'INSERT INTO users (name, password) VALUES (:username, :password)';
+            // パスワードハッシュ化
+            $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+            // プリペアドステートメント化
+            $stmt = $pdo->prepare($sql);
+            // ユーザー名をバインド
+            $stmt->bindParam(':username', $username);
+            // パスワードをバインド
+            $stmt->bindParam(':password', $password_hashed);
+            // 実行
+            $stmt->execute();
+            // 登録完了メッセージ出力
+            echo('New user registered.');
+        // 例外処理
+        } catch (PDOException $e) {
+            //エラーメッセージ表示
+            echo 'Error: ' . $e->getMessage();
+            // 接続終了
+            die();
+        }
     }
 }
 ?>
