cdcd153b768e3aa6f5bee2434acdb5661ed903de
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-06 18:16:09 +0900

    Update signUp.php
    phpを先、htmlを後に記述するよう修正

--- PHPjob/5/チェックテスト/signUp.php
+++ PHPjob/5/チェックテスト/signUp.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <div class="wrapper">
-        <h1>ユーザー登録画面</h1>
-        <a href="signIn.php">ログイン</a>
-    </div>
-    <form action="signUp.php" method="post">
-        <input type="text" placeholder='ユーザー名' name='username' id='username'><br>
-        <input type="password" placeholder='パスワード' name="password" id="password"><br>
-        <input type="submit" value="新規登録" id='signUp' name='signUp'>
-    </form>
-</body>
-</html>
-
 <?php
 // function.phpの読み込み
 require_once('function.php');
@@ -63,3 +42,25 @@ if(empty($_POST['signUp'])){
         die();
     }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>ユーザー登録画面</h1>
+        <a href="signIn.php">ログイン</a>
+    </div>
+    <form action="signUp.php" method="post">
+        <input type="text" placeholder='ユーザー名' name='username' id='username'><br>
+        <input type="password" placeholder='パスワード' name="password" id="password"><br>
+        <input type="submit" value="新規登録" id='signUp' name='signUp'>
+    </form>
+</body>
+</html>
