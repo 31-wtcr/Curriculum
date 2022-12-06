59fcebdafa734feb3fce74a9fc7a3e44af5427a3
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-06 16:49:31 +0900

    Update signUp.php
    ログイン画面へのリンク追加
    新規登録ボタンへのidとname付与
    新規登録処理記述

--- PHPjob/5/チェックテスト/signUp.php
+++ PHPjob/5/チェックテスト/signUp.php
@@ -7,11 +7,59 @@
     <title></title>
 </head>
 <body>
-    <h1>ユーザー登録画面</h1>
+    <div class="wrapper">
+        <h1>ユーザー登録画面</h1>
+        <a href="signIn.php">ログイン</a>
+    </div>
     <form action="signUp.php" method="post">
         <input type="text" placeholder='ユーザー名' name='username' id='username'><br>
         <input type="password" placeholder='パスワード' name="password" id="password"><br>
-        <input type="submit" value="新規登録">
+        <input type="submit" value="新規登録" id='signUp' name='signUp'>
     </form>
 </body>
 </html>
+
+<?php
+// function.phpの読み込み
+require_once('function.php');
+
+// $_POSTが空であれば処理を終了する
+if(empty($_POST['signUp'])){
+    return;
+// ユーザー名が空であれば警告を表示
+}elseif(empty($_POST['username'])) {
+    echo 'Username is empty.';
+// パスワードが空であれば警告を表示
+}elseif (empty($_POST['password'])) {
+    echo 'Password is empty.';
+
+// ユーザー名とパスワードが空でなければ以下の処理を続行
+}elseif(!empty($_POST['username']) && !empty($_POST['password'])){
+    // ユーザー名とパスワードのエスケープ処理
+    $username = htmlspecialchars($_POST['username'], ENT_QUOTES);
+    $password = htmlspecialchars($_POST['password'], ENT_QUOTES);
+    // ユーザー新規登録処理開始
+    $pdo = db_connect();
+    try {
+        // ユーザー名が一致するか検索するSQL文用意
+        $sql = 'INSERT INTO users (name, password) VALUES (:username, :password)';
+        // パスワードハッシュ化
+        $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+        // プリペアドステートメント化
+        $stmt = $pdo->prepare($sql);
+        // ユーザー名をバインド
+        $stmt->bindParam(':username', $username);
+        // パスワードをバインド
+        $stmt->bindParam(':password', $password_hashed);
+        // 実行
+        $stmt->execute();
+        // 登録完了メッセージ出力
+        echo('New user registered.');
+    // 例外処理
+    } catch (PDOException $e) {
+        //エラーメッセージ表示
+        echo 'Error: ' . $e->getMessage();
+        // 接続終了
+        die();
+    }
+}
