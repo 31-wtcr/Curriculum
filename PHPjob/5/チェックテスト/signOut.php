22e6b039911531b80155d4d1cb82b19f475dd483
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-12-07 13:45:18 +0900

    Update signOut.php
    ログアウト機能実装

---
+++ PHPjob/5/チェックテスト/signOut.php
@@ -0,0 +1,22 @@
+<?php
+// セッション開始
+session_start();
+// セッション変数のクリア
+$_SESSION = array();
+// セッションクリア
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h1>ログアウト画面</h1>
+    <p>ログアウトしました</p>
+    <a href="signIn.php">ログイン画面に戻る</a>
+</body>
+</html>
